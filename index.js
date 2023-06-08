acc5e8675cfe113dc0c229322e53847185c208ce
KIPROTYCH <kiprotych@gmail.com>
2023-06-08 13:50:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+// Function to return an array of thank you messages for each name provided in the function
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+// Function to count down from a given number from zero
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+countDown(5);
